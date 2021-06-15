6b278bbc9451e3f1c64dfdd2f112bbef94ceeaf1
BuildTools <unconfigured@null.spigotmc.org>
2021-06-15 17:00:19 -0400

    new java path and bootstrap path

--- core/views.py
+++ core/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'core/index.html')    
-def galery(request):
-    return render(request,'core/galeria.html')         
