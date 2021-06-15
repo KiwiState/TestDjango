bc1b40db9d55c242016d3eb666f9bdf703abb901
BuildTools <unconfigured@null.spigotmc.org>
2021-06-15 16:46:19 -0400

    new links core

--- core/views.py
+++ core/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'core/index.html')        
+    return render(request,'core/index.html')    
+def galery(request):
+    return render(request,'core/galeria.html')         
